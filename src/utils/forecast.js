bb58ecfdf5fa7521481aa683959743dace964159
Shuvadip Chattopadhyay <shuvadip45@gmail.com>
2022-03-24 16:45:19 +0530

    Modified forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback("Unable to find location!", undefined)
         }
         else{
-            callback(undefined, body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees. It feels like "+body.current.feelslike+" degrees.")
+            callback(undefined, body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees. It feels like "+body.current.feelslike+" degrees. Last updated: "+body.current.observation_time)
         }
     })
 }
